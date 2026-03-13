e86ff46558bf20e51d247b722134c383ef5a17bf
Dycyyy <138583632+Dycyyy@users.noreply.github.com>
2026-03-14 05:29:51 +0700

    Upload set/config.js

---
+++ set/config.js
@@ -0,0 +1,50 @@
+
+/**
+ * Jangan dijual.
+ * Dilarang menghapus credit developer.
+ *
+ * Developer : DyySilence
+ * Copyright © 2026
+ * Contact   : https://whatsapp.com/channel/0029Vb7uLYxIHphOIWOY8727
+ */
+// set/config.js
+global.owner        = "628997496333";
+global.namaOwner    = "Liam Punya Oca";
+global.linkOwner    = "https://instagram.com/yammayam_";
+global.prefix       = ".";
+global.botName      = "MieAyam";
+global.versionBot   = "v1.0.5";
+global.thumbnail    = "https://c.termai.cc/i164/cAhf.jpegg";
+global.jedaPushkontak = 6000;
+global.pairingNumber  = "237620877517";
+global.timezone     = "Asia/Jakarta";        
+global.groupUrl     = "https://instagram.com/yammayam_"; 
+global.websiteUrl   = "https://instagram.com/yammayam_"; 
+global.channelJid   = "120363425809110720@newsletter"; 
+global.channelName  = "MieAyam";    
+global.dev          = "Yam";           
+global.apiUrl = "https://api.dyysilence.biz.id/api";
+
+global.ptld = "https://";   // domain
+global.ptla = "ptla_";   // ptla
+global.ptlc = "ptlc_";        // ptlc
+
+global.ptln = "5";                          
+global.ptle = "15";                        
+global.ptll = "1";   
+
+global.dana      = "08997496333";
+global.gopay     = "-";
+global.bank      = "-";
+global.norek     = "-";
+global.atasNama  = "tanya owner";
+global.qrisUrl   = "-";
+
+global.mess = {
+  owner:    "lu siapa kocak",
+  premium:  "lah lau siape?",
+  group:    "di group kocak",
+  private:  "di pc woik",
+  admin:    "lah lau siape?",
+  botadmin: "adminin dulu kocak",
+};
